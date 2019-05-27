a7854a049c7c0e4180ad4d4bc7c2b45085af780a
seungdols <seungdols0822@gmail.com>
2019-05-27 22:13:05 +0900

    Modify meta data

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Seungdols Company`,
+  description: `Blog posted about Java, JavaScript, Docker, k8s, Spring framework etc`,
+  author: `[Seungdols]`,
+  introduction: `I'm interested in talking to other developers. So, I write post in my blog.`,
+  siteUrl: `https://seungdols.netlify.com`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `seungdols`, // Your Twitter account
+    github: `seungdols`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  keywords: [`blog`, `seungdols`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'seungdols', // Your disqus-short-name. check disqus.com.
+    utterances: 'seingdols/seungdols.dev', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'seungdols',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
