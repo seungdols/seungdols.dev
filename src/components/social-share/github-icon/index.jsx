e051e39fa06b898289e38484dd54182763cac99d
seungdols <seungdols0822@gmail.com>
2019-05-27 22:12:43 +0900

    Modify link Jbee github to seungdols repository

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/seungdols/seungdols.dev"
       className="github"
       aria-label="GitHub"
     >
