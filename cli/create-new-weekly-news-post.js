13dde251650858ca335049679f31dff13996533e
seungdols <seungdols0822@gmail.com>
2020-03-15 19:48:41 +0900

    Modify template of create weekly news

--- cli/create-new-weekly-news-post.js
+++ cli/create-new-weekly-news-post.js
@@ -9,23 +9,31 @@ const TARGET_DIR = `${cwd}${CONTENTS_DIR}`
 const DATE_WITH_TIMES_FORMAT = 'YYYY-MM-DD HH:MM:SS'
 const DATE_FORMAT = 'YYYY-MM-DD'
 
-
 const getNumberOfLastWeeklyNewsPost = () => {
-    return fs.readdirSync(TARGET_DIR, (err, items) => {
-            if (err) {
-                return console.log(`Unable to scan directory:  ${err}`);
-            }
-            return items
-        })
-        .filter(el => el.includes('weekly-news')).map(el => el.split('.').slice(0, -1).join('.')).map(el => (el.split('-').pop())).reduce((previos, current) => previos > current ? previos : current)
+  return fs
+    .readdirSync(TARGET_DIR, (err, items) => {
+      if (err) {
+        return console.log(`Unable to scan directory:  ${err}`)
+      }
+      return items
+    })
+    .filter(el => el.includes('weekly-news'))
+    .map(el =>
+      el
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+    )
+    .map(el => el.split('-').pop())
+    .reduce((previos, current) => (previos > current ? previos : current))
 }
 
-const getTemplate = (newNumberWeeklyNews) => {
-    return `
+const getTemplate = newNumberWeeklyNews => {
+  return `
 ---
 layout: post
-title: "그냥 저냥 #위클리뉴스 #${newNumberWeeklyNews}"
-description: "그냥 저냥 #위클리뉴스 #${newNumberWeeklyNews}"
+title: \"그냥 저냥 #위클리뉴스 #${newNumberWeeklyNews}\"
+description: \"그냥 저냥 #위클리뉴스 #${newNumberWeeklyNews}\"
 date: ${dateFns.format(new Date(), DATE_WITH_TIMES_FORMAT)}
 category: weekly-news
 tags: [weekly-news, programming]
@@ -34,24 +42,28 @@ comments: true
   `
 }
 
-const createWeeklyNewsPost = (lastNumber) => {
+const createWeeklyNewsPost = lastNumber => {
+  const newNumberWeeklyNews = Number(lastNumber) + 1
+  const fileName = `${dateFns.format(
+    new Date(),
+    DATE_FORMAT
+  )}-weekly-news-${newNumberWeeklyNews}`
 
-    const newNumberWeeklyNews = Number(lastNumber) + 1
-    const fileName = `${dateFns.format(new Date(), DATE_FORMAT)}-weekly-news-${newNumberWeeklyNews}`
+  fs.writeFile(
+    `${TARGET_DIR}/${fileName}.markdown`,
+    getTemplate(newNumberWeeklyNews),
+    err => {
+      if (err) {
+        log.error('Unknown Error: Cannot write file!')
+        return
+      }
 
-
-
-    fs.writeFile(`${TARGET_DIR}/${fileName}.markdown`, getTemplate(newNumberWeeklyNews), err => {
-        if (err) {
-            log.error('Unknown Error: Cannot write file!')
-            return
-        }
-
-        log.success('Success to create new post!')
-    })
+      log.success('Success to create new post!')
+    }
+  )
 }
 
 module.exports = (async function() {
-    const lastNumberWeeklyNews = getNumberOfLastWeeklyNewsPost()
-    createWeeklyNewsPost(lastNumberWeeklyNews)
+  const lastNumberWeeklyNews = getNumberOfLastWeeklyNewsPost()
+  createWeeklyNewsPost(lastNumberWeeklyNews)
 })()
